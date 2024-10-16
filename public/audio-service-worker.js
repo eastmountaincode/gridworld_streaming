13fad565878f7e26465a299c34f6adb27d2697de
eastmountaincode <andreweboylan@gmail.com>
2024-10-16 15:33:52 -0400

    service worker seems to work with just console logs

--- public/audio-service-worker.js
+++ public/audio-service-worker.js
@@ -8,13 +8,8 @@ self.addEventListener('activate', (event) => {
 
 self.addEventListener('message', (event) => {
     if (event.data && event.data.type === 'PLAY_AUDIO') {
-        // Start the foreground service
-        self.registration.showNotification('Audio Playing', {
-            body: `Now playing: ${event.data.track.title}`,
-            icon: 'favicon.ico',
-            tag: 'audio-playback'
-        });
+        console.log('Service Worker: Playing audio', event.data.track);
     } else if (event.data && event.data.type === 'TRACK_ENDED') {
-        console.log('Track ended, playing next track');
+        console.log('Service Worker: Track ended, playing next track');
     } 
 });
